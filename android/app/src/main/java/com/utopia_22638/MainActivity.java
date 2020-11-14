a0d81e68c03a1894d12c017bccf24336dcabd458
Crowdbotics <team@crowdbotics.com>
2020-11-14 23:27:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/utopia_22638/MainActivity.java
@@ -0,0 +1,15 @@
+package com.utopia_22638;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "utopia_22638";
+    }
+}
